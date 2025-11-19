c440adc21da83475a4ee1dbcbea430e888e56a86
MustaphaBousilDev <bousilmustapha@gmail.com>
2025-11-20 00:46:35 +0100

    creating Abstract Class AppError Handling

---
+++ services/orders/src/domain/errors/AppError.ts
@@ -0,0 +1,44 @@
+import { ErrorCode, getHttpStatusForErrorCode } from './ErrorCodes';
+
+export interface ErrorDetails {
+  [key: string]: any;
+}
+
+export abstract class AppError extends Error {
+    public readonly code: ErrorCode;
+    public readonly statusCode: number;
+    public readonly details?: ErrorDetails;
+    public readonly isOperational: boolean;
+    public readonly timestamp: string;
+
+    constructor(
+        code: ErrorCode,
+        message: string, 
+        details: ErrorDetails,
+        isOperational: boolean = true 
+    ){
+        super(message)
+        this.code = code;
+        this.statusCode = getHttpStatusForErrorCode(code);
+        this.details = details;
+        this.isOperational = isOperational;
+        this.timestamp = new Date().toISOString();
+
+        // Maintains proper stack trace for where our error was thrown
+        Error.captureStackTrace(this, this.constructor)
+        //Set the prototype explicitly
+        Object.setPrototypeOf(this, AppError.prototype);
+    }
+
+    toJSON(){
+        return {
+            code: this.code,
+            message: this.message,
+            statusCode: this.statusCode,
+            timestamp: this.timestamp,
+            details: this.details
+        }
+    }
+
+
+}
